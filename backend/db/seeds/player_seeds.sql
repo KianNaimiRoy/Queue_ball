92320f7b8f3004a8e9b9768d259c0acf1b6c68dc
Kian <kiannaimiroy@gmail.com>
2023-05-14 11:16:00 -0700

    add seeds

---
+++ backend/db/seeds/player_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO players(name, enqueued_at, is_admin, table_id)
+VALUES ('Booch Scootem', null, true, null),
+('Scooch Bootem', null, false, null),
+('Rick Dickles', null, false, null),
+('Luigi', null, false, null);
