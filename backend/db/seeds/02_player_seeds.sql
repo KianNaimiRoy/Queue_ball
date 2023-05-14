1132aeb39f1107c02239c6626bcc61ef957b5d02
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-14 16:23:53 -0700

    Rename file

---
+++ backend/db/seeds/02_player_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO players(name, enqueued_at, is_admin, table_id)
+VALUES ('Booch Scootem', null, true, null),
+('Scooch Bootem', null, false, null),
+('Rick Dickles', null, false, null),
+('Bingo', null, false, null),
+('Bongo', null, false, null),
+('Bango', null, false, null),
+('Luigi', null, false, null);
