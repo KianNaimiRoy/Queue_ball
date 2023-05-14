7f84192886c8f907b2bc14f1ea201a18e050884e
Kian <kiannaimiroy@gmail.com>
2023-05-14 12:24:26 -0700

    testing commits

--- backend/db/seeds/player_seeds.sql
+++ backend/db/seeds/player_seeds.sql
@@ -2,5 +2,6 @@ INSERT INTO players(name, enqueued_at, is_admin, table_id)
 VALUES ('Booch Scootem', null, true, null),
 ('Scooch Bootem', null, false, null),
 ('Rick Dickles', null, false, null),
+('Bingo', null, false, null),
 ('Bongo', null, false, null),
 ('Luigi', null, false, null);
