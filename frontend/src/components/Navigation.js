004f44591a62c38d375df93e755886c97d98eb12
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-15 10:49:56 -0700

    Initial commit

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import "./Navigation.scss";
+
+const Navigation = function () {
+  return (
+    <div class="navbar">
+      <div class="queue-ball">
+        <h1>QueueBall</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
