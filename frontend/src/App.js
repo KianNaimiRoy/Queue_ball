1d1f8a4fbe6137b0b280d5142c46e1a27389a5f0
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-15 11:38:03 -0700

    Import TableList component and App.scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import Navigation from "./components/Navigation";
+import TableList from "./components/TableList";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <TableList />
     </div>
   );
 }
