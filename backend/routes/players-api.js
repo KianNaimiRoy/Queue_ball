1d14f87ad892ebedb1f84b152b58425cb706159d
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-14 19:21:16 -0600

    add a patch and post route to players-api

--- backend/routes/players-api.js
+++ backend/routes/players-api.js
@@ -1,12 +1,30 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 
 //get all players
 router.get('/', (req, res) => {
-  res.json('Players API')
-})
+  res.json('Players API');
+});
+
+//get player/:id
+router.get('/:id', (req, res) => {
+  res.json('Players ID API');
+});
+
+//edit a player
+router.patch('/:id', (req, res) => {
+  const edit = req.body;
+  res.json({ edit });
+});
+
+//add a player
+router.post('/', (req, res) => {
+  res.json('Player Post');
+});
 
 
 
-module.exports = router
+
+
+module.exports = router;
