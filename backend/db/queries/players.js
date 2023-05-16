13be96eaa836307545461d601eafcc5f6b523414
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-16 10:38:55 -0700

    Create getPlayerCount helper method

--- backend/db/queries/players.js
+++ backend/db/queries/players.js
@@ -1,13 +1,26 @@
-const client = require('../connection');
+const client = require("../connection");
 
 //get all players
 const getAllPlayers = function() {
   return client
-    .query('SELECT * FROM players')
-    .then(players => {
+    .query("SELECT * FROM players")
+    .then((players) => {
       return players.rows;
     })
-    .catch(err => {
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getPlayerCount = function() {
+  return client
+    .query(
+      "SELECT count(name), table_id FROM players GROUP BY table_id ORDER BY table_id"
+    )
+    .then((players) => {
+      return players.rows;
+    })
+    .catch((err) => {
       console.log(err);
     });
 };
@@ -15,11 +28,11 @@ const getAllPlayers = function() {
 //get player by ID
 const getPlayerByID = function(id) {
   return client
-    .query('SELECT * FROM players WHERE id = $1;', [id])
-    .then(item => {
+    .query("SELECT * FROM players WHERE id = $1;", [id])
+    .then((item) => {
       return item.rows[0];
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
@@ -27,34 +40,11 @@ const getPlayerByID = function(id) {
 //add a player to the queue
 const enqueuePlayerByID = function(id, player) {
   return client
-    .query('UPDATE players SET name = $1, enqueued_at = NOW(), table_id = $2 WHERE id = $3 RETURNING *',
-      [player.name, player.table_id, id])
-    .then(player => {
-      return player.rows[0];
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
-//remove player from the queue
-const dequeuePlayerByID = function(id, player) {
-  return client
-    .query('UPDATE players SET name = $1, enqueued_at = null, table_id = null WHERE id = $2 RETURNING *',
-      [player.name, id])
-    .then(player => {
-      return player.rows[0];
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
-const addPlayer = function(player) {
-  return client
-    .query('INSERT INTO players (name, enqueued_at, is_admin, table_id) VALUES($1, null, false, null) RETURNING *',
-      [player.name])
-    .then(player => {
+    .query(
+      "UPDATE players SET name = $1, enqueued_at = NOW(), table_id = $2 WHERE id = $3 RETURNING *",
+      [player.name, player.table_id, id]
+    )
+    .then((player) => {
       return player.rows[0];
     })
     .catch((err) => {
@@ -62,10 +52,38 @@ const addPlayer = function(player) {
     });
 };
 
+//remove player from the queue
+const dequeuePlayerByID = function(id, player) {
+  return client
+    .query(
+      "UPDATE players SET name = $1, enqueued_at = null, table_id = null WHERE id = $2 RETURNING *",
+      [player.name, id]
+    )
+    .then((player) => {
+      return player.rows[0];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
+const addPlayer = function(player) {
+  return client
+    .query(
+      "INSERT INTO players (name, enqueued_at, is_admin, table_id) VALUES($1, null, false, null) RETURNING *",
+      [player.name]
+    )
+    .then((player) => {
+      return player.rows[0];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   getAllPlayers,
+  getPlayerCount,
   getPlayerByID,
   enqueuePlayerByID,
   dequeuePlayerByID,
