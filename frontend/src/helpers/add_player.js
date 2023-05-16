e5f63675df43ff6a63665be9d36c3bc5db3c4a9f
Kian <kiannaimiroy@gmail.com>
2023-05-16 12:02:08 -0700

    deny player if name is already in database and assign cookie if not

--- frontend/src/helpers/add_player.js
+++ frontend/src/helpers/add_player.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+function randomString() {
+  const length = 6;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  return Array.from({ length }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+}
+
 const addPlayer = function(name) {
   const player = {
     name: name,
@@ -9,20 +16,31 @@ const addPlayer = function(name) {
   };
 
   return axios.post(`/api/players/`, player)
-  .then(() => {
-    console.log('Player added to DB');
-    // Perform additional actions if needed
-  })
-  .catch(err => {
-    console.log('Error adding player:', err.message);
-    // Handle the error here
-    // Example: display the error message to the user
-    if (err.response && err.response.status === 400) {
-      alert('Error: ' + err.response.data);
-    } else {
-      alert('An error occurred while adding the player.');
-    }
-  });
-}; 
+    .then(() => {
+      console.log('Player added to DB');
+      //Give your user a cookie
+      //setting the cookie
+      localStorage.setItem(randomString(), JSON.stringify(player));
 
-export default addPlayer
+      //retrieve cookie, get item from localStorage
+      const playerCookie = localStorage.getItem(randomString());
+
+      //parsed stringified object
+      const parsedPlayerCookie = JSON.parse(playerCookie);
+    })
+    .catch(err => {
+      console.log('Error adding player:', err.message);
+      // Handle the error here to display to the user
+      if (err.response && err.response.status === 400) {
+        alert('Error: ' + err.response.data);
+      } else {
+        alert('An error occurred while adding the player.');
+      }
+    });
+
+  //storing the client-cookie in the browser
+
+
+};
+
+export default addPlayer;
