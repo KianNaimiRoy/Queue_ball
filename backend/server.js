ef56b6b6a313fd8cc2f24a8a6a8729b94d1b1702
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-15 20:28:43 -0700

    Add and use 'cors'

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 const PORT = process.env.PORT || 8080;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(cors());
 app.use(bodyParser.json());
 
+const playerApiRoutes = require("./routes/players-api");
+const tableApiRoutes = require("./routes/tables-api");
 
-const playerApiRoutes = require('./routes/players-api');
-const tableApiRoutes = require('./routes/tables-api');
-
-
-
-app.use('/api/players', playerApiRoutes);
-app.use('/api/tables', tableApiRoutes);
-
-
+app.use("/api/players", playerApiRoutes);
+app.use("/api/tables", tableApiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
