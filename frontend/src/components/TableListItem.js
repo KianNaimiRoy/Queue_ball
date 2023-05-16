f67fa5157c782ded1ceda476fa6ea25e2275420e
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-15 20:40:43 -0700

    Use props in jsx

--- frontend/src/components/TableListItem.js
+++ frontend/src/components/TableListItem.js
@@ -3,8 +3,8 @@ import "./TableListItem.scss";
 const TableListItem = function (props) {
   return (
     <div className="table-list-item">
-      <h1>Table 1</h1>
-      <p>6</p>
+      <h1>{props.name}</h1>
+      <p>{props.id}</p>
     </div>
   );
 };
