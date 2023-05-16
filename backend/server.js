83b3a45954d85d60d1a0e0dc8143d6f50c6bac0f
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-15 22:38:39 -0600

    add cors

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ const app = express();
 const morgan = require('morgan');
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 const playerApiRoutes = require('./routes/players-api');
