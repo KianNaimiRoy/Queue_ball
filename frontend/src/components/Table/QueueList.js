64097eed08c75a628187c3010fe817b979c7f132
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-17 16:21:10 -0600

    update player's queue in real time when a player joins the queue

--- frontend/src/components/Table/QueueList.js
+++ frontend/src/components/Table/QueueList.js
@@ -7,35 +7,37 @@ import QueueListItem from "./QueueListItem";
 const QueueList = function(props) {
   const [players, setPlayers] = useState([]);
   const [socket, setSocket] = useState([]);
-  // const [playerName, setPLayerName] = useState('');
 
   useEffect(() => {
     axios.get("/api/players").then((response) => {
       const players = response.data.players;
       setPlayers(players);
+      // console.log('State of Players: ', players)
     });
 
     const socket = io("http://localhost:3000/");
     setSocket(socket);
 
-
     socket.on("connect", () => {
       console.log("Connected", socket.id);
-      //initial send to the server
-      socket.emit("test", "Hello World");
     });
     socket.on("connected_error", () => {
       setTimeout(() => socket.connect(), 5000);
     });
 
     socket.on("public", (player) => {
-      console.log(`Player ${player} just joined the queque!`);
-        setPlayers((prev) => [...prev, player]);
+      console.log(`Player ${player} just joined the queue!`);
+      setPlayers((prev) => [...prev, player]);
     });
-
+    
     //clean up  to prevent memory leak
     return () => socket.disconnect();
   }, []);
+  
+  useEffect(() => {
+    //render listPlayers?
+  console.log("players updated: ", players)
+  }, [players])
 
   const listPlayers = players.map((player) => {
     return <QueueListItem key={player.id} id={player.id} name={player.name} />;
@@ -43,19 +45,18 @@ const QueueList = function(props) {
 
   const joinQueue = () => {
     const playerInSession = localStorage.getItem("player-data");
-    const playerName = JSON.parse(playerInSession).name;
-
+    const playerName = JSON.parse(playerInSession);
+    
     socket.emit("player-name", playerName);
-    window.location.reload(); 
   };
 
   return (
     <section>
       <div className="queue-list">
-        {listPlayers}
-        <div className="queue-list-item">
+        <>{listPlayers}</>
+        <h1>
           <button type="submit" onClick={joinQueue}>Join the Queue</button>
-        </div>
+        </h1>
       </div>
     </section>
   );
