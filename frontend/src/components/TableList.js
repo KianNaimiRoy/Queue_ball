1f6e58dc241201f635daa5162a97089983426573
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-16 20:46:28 -0600

    sync the localStorage and state

--- frontend/src/components/TableList.js
+++ frontend/src/components/TableList.js
@@ -1,42 +1,32 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./TableList.scss";
 import TableListItem from "./TableListItem";
 
 const TableList = function (props) {
   const [state, setState] = useState({
-    focused: null,
+    focused: JSON.parse(localStorage.getItem("focused")),
     tables: []
   });
 
   useEffect(() => {
-    const focused = JSON.stringify(localStorage.getItem("focused"));
-
     axios.get("/api/players/count").then((response) => {
       setState((prevState) => ({ ...prevState, tables: response.data.count }));
     });
-
-    if (focused) {
-      // localStorage.setItem("focused", JSON.stringify(state.focused));
-      setState((prevState) => ({ ...prevState, focused }));
-    } else {
-      localStorage.setItem("focused", JSON.stringify(state.focused));
-    }
-
-    console.log("Local storage focused", focused);
   }, []);
 
   const selectTable = function (id) {
-    setState((prevState) => {
-      const newFocused = prevState.focused !== null ? null : id;
-      localStorage.setItem("focused", JSON.stringify(newFocused));
-      return {
-        ...prevState,
-        focused: newFocused
-      };
-    });
-    console.log("Focused State: ", state.focused);
+    const newFocused = state.focused !== id ? id : null;
+    localStorage.setItem("focused", JSON.stringify(newFocused));
+    setState((prevState) => ({
+      ...prevState,
+      focused: newFocused
+    }));
   };
+
+  useEffect(() => {
+    console.log("Focused State: ", state.focused);
+  }, [state.focused]);
 
   const listTables = state.tables.map((table) => {
     return (
