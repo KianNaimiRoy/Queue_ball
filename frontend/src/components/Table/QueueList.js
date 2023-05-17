082099777c26d8bc2d6fda7727ae1ff219e79a05
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-16 22:39:00 -0600

    set up client connection to the server with socket.io

--- frontend/src/components/Table/QueueList.js
+++ frontend/src/components/Table/QueueList.js
@@ -1,24 +1,55 @@
 import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 import axios from "axios";
-
 import "./QueueList.scss";
 import QueueListItem from "./QueueListItem";
 
-const QueueList = function (props) {
+const QueueList = function(props) {
   const [players, setPlayers] = useState([]);
+  const [socket, setSocket] = useState();
+
+  // useEffect(() => {
+  //   axios.get("/api/players").then((response) => {
+  //     console.log("Response.data: ", response.data.players);
+  //     setPlayers(response.data.players);
+  //   });
+  // }, []);
 
   useEffect(() => {
     axios.get("/api/players").then((response) => {
-      console.log("Response.data: ", response.data.players);
-      setPlayers(response.data.players);
+      const players = response.data.players;
+      setPlayers(players);
     });
+
+    const socket = io("http://localhost:3000/");
+    setSocket(socket);
+
+    socket.on("connect", () => {
+      console.log("Connected", socket.id);
+    });
+    socket.on("connected_error", () => {
+      setTimeout(() => socket.connect(), 5000);
+    });
+
+    //initial send to the server
+    socket.emit("test", "Hello World");
+
+    //send list of players to the server
+    socket.emit("players-list", players);
+    console.log("Initial players list", players);
+
+    //clean up  to prevent memory leak
+    return () => socket.disconnect();
   }, []);
 
-  console.log("Players:", players);
+
+  // console.log("Players:", players);
 
   const listPlayers = players.map((player) => {
     return <QueueListItem key={player.id} id={player.id} name={player.name} />;
   });
+
+
 
   return <div className="queue-list">{listPlayers}</div>;
 };
