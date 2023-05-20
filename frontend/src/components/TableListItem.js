7d801546ce75374ad35f4f8c6bdb66331ddf1ab2
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-19 19:00:27 -0700

    Remove commented-out code

--- frontend/src/components/TableListItem.js
+++ frontend/src/components/TableListItem.js
@@ -1,6 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { io } from "socket.io-client";
-// import axios from "axios";
 import classNames from "classnames";
 import QueueListItem from "./Table/QueueListItem";
 import Button from "./Button";
@@ -9,40 +6,6 @@ import useTableListItem from "./hooks/useTableListItems";
 const TableListItem = function (props) {
   const { players, joinQueue, leaveQueue, isTableIdNull, playerTableNumber } =
     useTableListItem(props);
-
-  // const [players, setPlayers] = useState([]);
-  // const [socket, setSocket] = useState([]);
-
-  // useEffect(() => {
-  //   axios.get("/api/players").then((response) => {
-  //     setPlayers(response.data.players);
-  //   });
-
-  //   const socket = io("http://localhost:3000/");
-  //   setSocket(socket);
-
-  //   socket.on("connect", () => {
-  //     console.log("Connected", socket.id);
-  //   });
-  //   socket.on("connected_error", () => {
-  //     setTimeout(() => socket.connect(), 5000);
-  //   });
-
-  //   socket.on("enqueue", (player) => {
-  //     setPlayers((prev) => [...prev, player]);
-  //   });
-
-  //   socket.on("dequeue", (player) => {
-  //     setPlayers((prev) => prev.filter((p) => p.name !== player.name));
-  //   });
-
-  //   socket.on("table-update", (tables) => {
-  //     props.updateTables(tables);
-  //   });
-
-  //   //clean up  to prevent memory leak
-  //   return () => socket.disconnect();
-  // }, []);
 
   const listPlayers = players.filter(
     (players) => props.focused === players.table_id
@@ -70,37 +33,6 @@ const TableListItem = function (props) {
   const remainingPlayerItems = remainingPlayers.map((player) => (
     <QueueListItem key={player.id} name={player.name} />
   ));
-
-  // const joinQueue = () => {
-  //   const playerObj = JSON.parse(localStorage.getItem("player-data"));
-  //   playerObj.table_id = props.id;
-  //   localStorage.setItem("player-data", JSON.stringify(playerObj)); // Update table_id in localStorage
-  //   socket.emit("enqueue", playerObj);
-  //   axios.patch("/api/players/enqueued", playerObj).then((response) => {
-  //     socket.emit("table-update", response.data.tables);
-  //     props.updateTables(response.data.tables);
-  //   });
-  // };
-
-  // const leaveQueue = () => {
-  //   const playerObj = JSON.parse(localStorage.getItem("player-data"));
-  //   playerObj.table_id = null; // Update table_id in localStorage(set it to null)
-  //   localStorage.setItem("player-data", JSON.stringify(playerObj));
-  //   socket.emit("dequeue", playerObj);
-  //   axios.patch("/api/players/dequeued", playerObj).then((response) => {
-  //     socket.emit("table-update", response.data.tables);
-  //     props.updateTables(response.data.tables);
-  //   });
-  // };
-
-  // const isTableIdNull = localStorage.getItem("player-data")
-  // ? JSON.parse(localStorage.getItem("player-data")).table_id === null
-  // : false;
-
-  // //retrieve the table_id number that a player has joined
-  // const playerTableNumber = localStorage.getItem("player-data")
-  // ? JSON.parse(localStorage.getItem("player-data")).table_id
-  // : null;
 
   const listClass = classNames("table-list__item", {
     "table-list__unavailable": !props.status
