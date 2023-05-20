5be64f9d187007cdb6cfa9d5782766ca2625c2d2
Kian <kiannaimiroy@gmail.com>
2023-05-19 18:16:35 -0700

    start refactoring

--- frontend/src/components/Table/QueueListItem.js
+++ frontend/src/components/Table/QueueListItem.js
@@ -2,7 +2,7 @@ import "./QueueListItem.scss";
 
 const QueueListItem = function (props) {
   return (
-    <div className="queue-list-item">
+    <div className={`queue-list-item ${props.className}`}>
       <h1>{props.name}</h1>
     </div>
   );
