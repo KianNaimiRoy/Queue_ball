afbb5e0629d25942d7c48bb6c29ddf289027ec73
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-20 19:42:36 -0600

    add UsernamePrompt component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import TableList from "./components/TableList";
+import UsernamePrompt from "./components/UsernamePrompt";
+
+
 import "./App.scss";
 
 const App = function() {
   return (
     <main className="App">
+      <UsernamePrompt />
       <Navigation />
       <TableList />
     </main>
