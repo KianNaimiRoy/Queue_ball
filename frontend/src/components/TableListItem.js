d5a11e2bb472c1b87652e144b25a1b5bbc5c392b
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-22 13:14:52 -0600

    conditionally render ball colours

--- frontend/src/components/TableListItem.js
+++ frontend/src/components/TableListItem.js
@@ -45,6 +45,17 @@ const TableListItem = function(props) {
     "data-fade-in": props.fadeIn
   });
 
+  const listBallColours = classNames("players-at-table-count", {
+    "player-count-one": props.playerCount === "1" && props.status,
+    "player-count-two": props.playerCount === "2" && props.status, 
+    "player-count-three": props.playerCount === "3" && props.status,
+    "player-count-four": props.playerCount === "4" && props.status,
+    "player-count-five": props.playerCount === "5" && props.status,
+    "player-count-six": props.playerCount === "6" && props.status,
+    "player-count-seven": props.playerCount === "7" && props.status,
+    "player-count-eight": props.playerCount === "8" && props.status
+  });
+
   return (
     <div className={listClass} onClick={props.status ? props.onSelect : null}>
       {props.focused ? (
@@ -97,7 +108,11 @@ const TableListItem = function(props) {
         <>
           <h1>{props.name}</h1>
           <div className="players-at-table">
-            <p>{!props.status ? "Unavailable" : props.playerCount}</p>
+            <div className={listBallColours}>
+              <div className="inner-ball-circle">
+                {!props.status ? "Unavailable" : props.playerCount}
+              </div>
+            </div>
             <h3>{props.status && "Players at Table"}</h3>
           </div>
         </>
