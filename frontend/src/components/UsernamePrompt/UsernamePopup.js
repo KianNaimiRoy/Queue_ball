0d3aa9b28f7ec79394c2fdc4e6babe1e389e90cc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-23 19:27:16 -0700

    Edit welcome message

--- frontend/src/components/UsernamePrompt/UsernamePopup.js
+++ frontend/src/components/UsernamePrompt/UsernamePopup.js
@@ -14,13 +14,10 @@ const UsernamePopup = function (props) {
             </h1>
           </div>
           <div className="welcome-message">
-            We're excited to introduce a modern way to reserve your spot at the
-            pool table without the hassle of hunting for coins or waiting in
-            line. All you need to do is simply scan the QR code near your table
-            of choice and add yourself to the queue. To get started, enter a
-            unique handle below.
+            Introducing a modern way to reserve your spot at the pool table
+            without the hassle of hunting for coins or waiting in line. To get
+            started, enter a unique handle below.
           </div>
-          {/* <span className="close-icon" onClick={props.onClose}>x</span> */}
           <Form onClose={props.onClose} />
         </div>
       </div>
